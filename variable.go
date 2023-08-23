908a4bb5ec8f57c27b545872c97b7b39a29ae514
nacanz <naca.anggitian22@gmail.com>
2023-08-23 20:40:32 +0700

    Feat: variable

---
+++ variable.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Chasa Name"
+	fmt.Println(name)
+
+	name = "Chasa Name Again"
+	fmt.Println(name)
+
+	var friendName = "Farel"
+	fmt.Println(friendName)
+
+	var age = 30
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	country = "Amerika"
+	fmt.Println(country)
+
+	var (
+		firstName = "Chasa"
+		lastName  = "Name"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
