76d4d8872e6fd619a0f7ffd3f14b7b1aac97fea4
nacanz <naca.anggitian22@gmail.com>
2023-09-17 15:39:55 +0700

    feat: functino anonymous

---
+++ function-anonymous.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type BlackList func(string) bool
+
+func registerUser(name string, blackList BlackList) {
+	if blackList(name) {
+		fmt.Println("You are blocked", name)
+	} else {
+		fmt.Println("Welcome", name)
+	}
+}
+
+// func blackListAdmin(name string) bool {
+// 	return name == "admin"
+// }
+
+// func blackListRoot(name string) bool {
+// 	return name == "root"
+// }
+
+func main() {
+	blackList := func(name string) bool {
+		return name == "admin"
+	}
+
+	registerUser("admin", blackList)
+	registerUser("chasa", blackList)
+
+	registerUser("root", func(name string) bool {
+		return name == "root"
+	})
+}
