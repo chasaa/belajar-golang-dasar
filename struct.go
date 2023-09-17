da95f178dddfafaee011a1577db2a2d0a32309f5
nacanz <naca.anggitian22@gmail.com>
2023-09-17 16:45:44 +0700

    feat: struct method

--- struct.go
+++ struct.go
@@ -7,11 +7,22 @@ type Customer struct {
     Age int
 }
 
+func (customer Customer) sayHi(name string) {
+    fmt.Println("Hello", name, "My Name is", customer.Name)
+}
+
+func (customer Customer) sayHuuu() {
+    fmt.Println("Huuu from", customer.Name)
+}
+
 func main() {
     var chasa Customer
     chasa.Name = "Chasa"
     chasa.Address = "Indonesia"
     chasa.Age = 30
+
+    chasa.sayHi("Joko")
+    chasa.sayHuuu()
 
     fmt.Println(chasa.Name)
     fmt.Println(chasa.Address)
